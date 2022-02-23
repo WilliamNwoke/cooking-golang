3b05d8d6257757b133766a9a2e7e028d4f5e31ba
William Nwoke <willteey@gmail.com>
2022-02-23 14:56:15 -0800

    Added comments to explain the code

--- CGGP/MadLibs/MadLibs.go
+++ CGGP/MadLibs/MadLibs.go
@@ -1,3 +1,11 @@
+/**
+Madlibs program is a software that takes in a users inputs and places
+these inputs into a String, then prints it out
+
+@author: Uchenna Nwoke
+@version: 1.0
+
+*/
 package main
 
 import (
@@ -6,9 +14,20 @@ import (
 	"os"
 )
 
+/**
+This is the main method, which creates a scanner object to accept input from
+standard in. These inputs are stored in variables and used in a setence.
+
+*/
 func main() {
-	//  scanner object
+	//  scanner object to read from Standard input
 	scanner := bufio.NewScanner(os.Stdin)
+
+	/*
+		Prompts a question to standard out
+		Scans for any entries made in the standard in (command line/terminal)
+		Passes those values as string and stores in the variable
+	*/
 	fmt.Print("Enter an adjective: ")
 	scanner.Scan()
 	adj := scanner.Text()
@@ -25,8 +44,10 @@ func main() {
 	scanner.Scan()
 	famousPerson := scanner.Text()
 
+	// Using the fmt.Sprintf to format the string
 	madlibs := fmt.Sprintf("Computer pramming is so %s! It makes me so excited all the time because \n"+
 		" I love to %s. Stay hydrated and %s like you are %s", adj, verb1, verb2, famousPerson)
 
+	// Prints madlibs to standard out
 	fmt.Printf(madlibs)
 }
